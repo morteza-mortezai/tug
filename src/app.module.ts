b5f7e3fd8d13835b5874f7d09c128749883a9489
morteza-mortezai <mrtz.mrtzi.1989@gmail.com-at-003669155861>
2025-04-05 15:53:24 +0330

    add product resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompanyModule } from './company/company.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [CompanyModule],
+  imports: [CompanyModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
