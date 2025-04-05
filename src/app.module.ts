dd9b285ddf02968b857a858dd24c63f4656c99ec
morteza-mortezai <mrtz.mrtzi.1989@gmail.com-at-003669155861>
2025-04-05 17:12:56 +0330

    config service added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompanyModule } from './company/company.module';
 import { ProductModule } from './product/product.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmAsyncConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [CompanyModule, ProductModule],
+  imports: [
+    CompanyModule,
+    ProductModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,17 @@
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory:  (configService: ConfigService) => ({
+    type: 'mysql',
+    host: configService.get('DB_HOST'),
+    port: configService.get<number>('DB_PORT'),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    synchronize: configService.get('NODE_ENV') !== 'production', // disable in production
+  }),
+};
