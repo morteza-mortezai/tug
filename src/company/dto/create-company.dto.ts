39c4088a114885388ecb1fbd10b349dba93d3825
morteza-mortezai <mrtz.mrtzi.1989@gmail.com-at-003669155861>
2025-04-06 15:50:28 +0330

    improve dto

--- src/company/dto/create-company.dto.ts
+++ src/company/dto/create-company.dto.ts
@@ -1,8 +1,17 @@
 import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCompanyDto {
+  @ApiProperty({
+    description: 'The name of the company',
+    example: 'Company Name',
+  })
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'The product ID associated with the company',
+    example: 1,
+  })
   productId: number;
 }
--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -5,20 +5,31 @@ import {
   IsNotEmpty,
   Matches,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'The name of the product',
+    example: 'Sample Product',
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'The barcode of the product',
+    example: '1234567890123',
+  })
   @Transform(({ value }: { value: unknown }) => String(value))
   @Matches(/^\d+$/, { message: 'Barcode must be a numeric string' })
   barcode: string;
 
+  @ApiProperty({ description: 'The category ID of the product', example: 1 })
   @IsInt()
   categoryId: number;
 
+  @ApiProperty({ description: 'The subcategory ID of the product', example: 1 })
   @IsOptional()
   @IsInt()
   subcategoryId?: number;
